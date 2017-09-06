2ab33ac058d94d99364f8fe4d3e8e9793d21f99b
Carl Johan Hambro <carljohan.hambro@gmail.com>
2017-09-06 15:34:15 +0200

    Add simple admin panel for CRUD operations on instructor models

--- instructor/admin.py
+++ instructor/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Medicine, PatientTreatmentPlan, Disease, Medium, Plan, PlanDosage, UserDose
+
+
+class PlanDosageInline(admin.TabularInline):
+    model = PlanDosage
+
+
+class PlanAdmin(admin.ModelAdmin):
+    inlines = [PlanDosageInline, ]
+
+
+admin.site.register(Medicine)
+admin.site.register(PatientTreatmentPlan)
+admin.site.register(Disease)
+admin.site.register(Medium)
+admin.site.register(Plan, PlanAdmin)
+
+admin.site.register(UserDose)
